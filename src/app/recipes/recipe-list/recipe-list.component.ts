eb88928e67317ce6f6011541d32fc3f531725091
TeniGoldie <ta.zharska@gmail.com>
2017-07-17 17:46:51 +0300

    New commit. Added Recipes details view and Shopping List add option

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.module';
 
 @Component({
@@ -7,9 +7,10 @@ import { Recipe } from '../recipe.module';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a description', 'http://www.downgraf.com/wp-content/uploads/2013/06/Logo-Design-Inspiration-12.jpg'),
-    new Recipe('A test recipe', 'This is a description', 'https://cdn.dribbble.com/users/301004/screenshots/994795/dribble.jpg')
+  new Recipe('A test recipe', 'This is a description', 'http://www.downgraf.com/wp-content/uploads/2013/06/Logo-Design-Inspiration-12.jpg'),
+  new Recipe('Another test recipe', 'This is some description', 'https://cdn.dribbble.com/users/301004/screenshots/994795/dribble.jpg')
   ];
 
   constructor() { }
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
